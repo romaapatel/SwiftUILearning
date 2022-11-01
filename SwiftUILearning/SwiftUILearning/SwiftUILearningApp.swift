91ba234bf1cac54c3a22a51655896b835cfd27e4
Roma Patel <roma.p@simformsolutions.com>
2022-11-01 12:24:49 +0530

    project setup

---
+++ SwiftUILearning/SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Roma Patel on 01/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
